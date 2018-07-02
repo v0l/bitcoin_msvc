9428813b9b8fefddf670d827094a6ab5311837fa
Kieran <kieran@harkin.me>
2018-07-03 01:05:57 +0800

    step

--- build.cmd
+++ build.cmd
@@ -3,10 +3,9 @@
 copy /Y "CMakeLists_leveldb.txt" "bitcoin/src/leveldb/CMakeLists.txt"
 copy /Y "CMakeLists_secp256k1.txt" "bitcoin/src/secp256k1/CMakeLists.txt"
 copy /Y "CMakeLists_univalue.txt" "bitcoin/src/univalue/CMakeLists.txt"
+copy /Y "libsecp256k1-config.h.in" "bitcoin/src/secp256k1/libsecp256k1-config.h.in"
 
 mkdir build
 cd build
 
-cmake .. -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake
-
-msbuild bitcoin.sln /p:Configuration=Release
+cmake .. -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\\scripts\\buildsystems\\vcpkg.cmake"
