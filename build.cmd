72dd9113d8c895ee3a88f13296d99ca0dea65df5
Kieran <kieran@harkin.me>
2018-07-03 18:37:50 +0800

    clean build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+git clone https://github.com/bitcoin/bitcoin.git
+
+vcpkg install leveldb libevent openssl berkeleydb boost-system boost-filesystem	boost-chrono boost-program-options boost-test boost-thread boost-signals boost-signals2 boost-multi-index boost-interprocess
+
 copy /Y "CMakeLists_leveldb.txt" "bitcoin/src/leveldb/CMakeLists.txt"
 copy /Y "CMakeLists_secp256k1.txt" "bitcoin/src/secp256k1/CMakeLists.txt"
 copy /Y "CMakeLists_univalue.txt" "bitcoin/src/univalue/CMakeLists.txt"
